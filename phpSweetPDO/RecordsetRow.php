28c959353207600e8af10c78ce560dd77d7afe8a
Vladislav Rastrusny <FractalizeR@yandex.ru>
2011-11-08 18:06:03 +0400

    RecordsetRow now implements ArrayAccess to function as array Some minor configuration files changed

--- phpSweetPDO/RecordsetRow.php
+++ phpSweetPDO/RecordsetRow.php
@@ -25,9 +25,87 @@ namespace phpSweetPDO;
  * to get non-existing field's value
  *
  */
-class RecordsetRow {
 
+class RecordsetRow implements \ArrayAccess {
+
+    /**
+     * Implemented to prevent reading inexisting fields
+     *
+     * @param $name
+     *
+     * @throws \LogicException
+     */
     public function __get($name) {
         throw new \LogicException('Attempt to read nonexistent recordset field: ' . $name);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Whether a offset exists
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     * </p>
+     *
+     * @return boolean Returns true on success or false on failure.
+     * </p>
+     * <p>
+     *       The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset) {
+        return isset($this->$offset);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Offset to retrieve
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     * </p>
+     *
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset) {
+        return $this->$offset;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Offset to set
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to assign the value to.
+     * </p>
+     * @param mixed $value  <p>
+     *                      The value to set.
+     * </p>
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value) {
+        $this->$offset = $value;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Offset to unset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     * </p>
+     *
+     * @return void
+     */
+    public function offsetUnset($offset) {
+        unset($this->$offset);
     }
 }
